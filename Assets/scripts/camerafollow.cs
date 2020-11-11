b88050633a3dc6609790cd82b14de2d5f9a84616
freddiecooper <72386496+freddiecooper@users.noreply.github.com>
2020-11-11 12:38:55 +0000

    mmm

---
+++ Assets/scripts/camerafollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("player").transform;
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+
+        temp.x = playerTransform.position.x;
+
+        temp.y = playerTransform.position.y;
+
+        transform.position = temp;
+    }
+}
--- Assets/scripts/enemyscript.cs
+++ Assets/scripts/enemyscript.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class enemyscript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+
+    private Transform target;
+
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag("player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
     
 }
